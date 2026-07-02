5e502aed7be8ac052677eaf040ff1fa3c1e5b3ee
andrewdark <samsonov.a@ukr.net>
2026-07-02 17:00:54 +0300

    Init commit

---
+++ darkystore/src/main/java/ua/pp/darknsoft/darkystore/DarkystoreApplication.java
@@ -0,0 +1,13 @@
+package ua.pp.darknsoft.darkystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkystoreApplication.class, args);
+	}
+
+}
---
+++ darkystore/src/test/java/ua/pp/darknsoft/darkystore/DarkystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.pp.darknsoft.darkystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
